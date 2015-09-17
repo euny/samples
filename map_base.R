776bcac16118abc5d7f40fd89f98014210ff6f0b
euny <deadbird0@gmail.com>
2015-09-17 22:43:44 +0900

    markdown 양식 추가, 네이버 지도 위로 올림

--- map_base.R
+++ map_base.R
@@ -1,33 +1,28 @@
-### 지도 샘플 
+#+ global_options, echo = F, results = "hide" 
+library(knitr)
+opts_chunk$set(cache=FALSE)
+options(width=120)
+
+#' ---
+#' title: '지도 밑바탕 그리기'
+#' css: custom.css
+#' ---
+
+#' ## 라이브러리  
 library(ggmap)
 
-# Google 
+#' ## Google 
 qmap( location = "서울" , zoom = 12 , maptype = "terrain")
 qmap( location = "서울" , zoom = 12 , maptype = "satellite")
 qmap( location = "서울" , zoom = 12 , maptype = "hybrid")
 qmap( location = "서울" , zoom = 12 , maptype = "roadmap")
 qmap( location = "서울" , zoom = 12 , maptype = "roadmap", color = "bw")  # 흑백 
 
-# OpenStreetMap
-qmap( location = "서울" , zoom = 12, source = "osm") # 한참 걸려요... 
 
-# Stamen http://maps.stamen.com/
-qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner")
-qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner-background")
-qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner-lite")
-qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner-hybrid")
-qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner-labels")
-qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner-lines")
-qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner-2010")
-qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner-2011")
-qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "watercolor")
-
-qmap( location = "Newyork" , zoom = 12 , source = "stamen", maptype = "terrain-background")  # 미국만 됨 
-qmap( location = "Newyork" , zoom = 12 , source = "stamen", maptype = "terrain-labels")      # 미국만 됨 
-qmap( location = "Newyork" , zoom = 12 , source = "stamen", maptype = "terrain-lines")      # 미국만 됨 
-qmap( location = "Newyork" , zoom = 12 , source = "stamen", maptype = "terrain")             # 미국만 됨 
-
-# NaverMap 
+#' ## NaverMap 
+#' * key 를 발급받아야 해서 번거롭다.
+#' * overlayers 를 하나도 깔지 않은 지도를 그릴 수 없다?!
+#' 
 ggmap( get_navermap(key="967590eb29f58ed9c53ba5ecd3452695", uri="www.r-project.org", 
                     location = "서울", zoom = 6, baselayer = "default", overlayers = "traffic") )
 ggmap( get_navermap(key="967590eb29f58ed9c53ba5ecd3452695", uri="www.r-project.org", 
@@ -39,3 +34,25 @@ ggmap( get_navermap(key="967590eb29f58ed9c53ba5ecd3452695", uri="www.r-project.o
 ggmap( get_navermap(key="967590eb29f58ed9c53ba5ecd3452695", uri="www.r-project.org", 
                     location = "서울", zoom = 6, baselayer = "satellite", overlayers = "anno_satellite") )
 
+
+#' ## OpenStreetMap
+qmap( location = "서울" , zoom = 12, source = "osm") # 한참 걸려요... 
+
+
+#' ## Stamen 
+#' http://maps.stamen.com/ 에서도 지도를 확인할 수 있다. 
+qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner")
+qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner-background")
+qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner-lite")
+qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner-hybrid")
+qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner-labels")
+qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner-lines")
+qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner-2010")
+qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "toner-2011")
+qmap( location = "Seoul" , zoom = 12 , source = "stamen", maptype = "watercolor")
+
+#' 아래 지도는 미국만 됨 
+qmap( location = "Newyork" , zoom = 12 , source = "stamen", maptype = "terrain-background")   
+qmap( location = "Newyork" , zoom = 12 , source = "stamen", maptype = "terrain-labels")       
+qmap( location = "Newyork" , zoom = 12 , source = "stamen", maptype = "terrain-lines")       
+qmap( location = "Newyork" , zoom = 12 , source = "stamen", maptype = "terrain")              
